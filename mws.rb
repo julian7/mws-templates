2fba7d69f4ec3aff8d95d8874c4b9e7127f4bf61
Balazs Nagy <julsevern@gmail.com>
2009-04-24 00:44:58 +0200

    Indentation changes, gem and plugin changes
    Change from tab indentation to 2-space soft tabs.
    
    Semantic form builder is proven to be inferior, I changed it to formtastic,
    which is more promising.
    
    I added cucumber generation and factory-girl to the gem requirements.
    
    Signed-off-by: Balazs Nagy <julsevern@gmail.com>

--- mws.rb
+++ mws.rb
@@ -36,24 +36,28 @@ gem "sqlite3-ruby", :lib => "sqlite3", :version => ">=1.2.3"
 gem "vlad", :lib => false, :version => ">=1.3.2"
 
 with_options :env => "test" do |test|
-	test.gem "rspec", :lib => false, :version => ">=1.2.2"
-	test.gem "rspec-rails", :lib => false, :version => ">=1.2.2"
-	test.gem "webrat", :lib => false, :version => ">=0.4.3"
-	test.gem "cucumber", :lib => false, :version => ">=0.3.0"
+  test.gem "rspec", :lib => false, :version => ">=1.2.2"
+  test.gem "rspec-rails", :lib => false, :version => ">=1.2.2"
+  test.gem "webrat", :lib => false, :version => ">=0.4.3"
+  test.gem "cucumber", :lib => false, :version => ">=0.3.0"
+  test.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => "http://gems.github.com", :version => '>=1.2.1'
 end
 
 rake('gems:install')
 rake('gems:install', :env => "test")
-
+generate('cucumber')
 
 git :init
 
 with_options :submodule => true do |sub|
-	sub.plugin 'semantic_form_builder', :git => 'git://github.com/rubypond/semantic_form_builder.git'
-	sub.plugin 'asset_packager', :git => 'git://github.com/sbecker/asset_packager.git'
-	sub.plugin "paperclip", :git => "git://github.com/thoughtbot/paperclip.git"
-	sub.plugin 'restful-authentication', :git => 'git://github.com/technoweenie/restful-authentication.git'
-	sub.plugin 'role_requirement', :git => 'git://github.com/timcharper/role_requirement.git'
+# sub.plugin 'semantic_form_builder', :git => 'git://github.com/rubypond/semantic_form_builder.git'
+  sub.plugin 'formtastic', :git => 'git://github.com/justinfrench/formtastic.git'
+  sub.plugin 'asset_packager', :git => 'git://github.com/sbecker/asset_packager.git'
+  sub.plugin 'paperclip', :git => 'git://github.com/thoughtbot/paperclip.git'
+  sub.plugin 'clearance', :git => 'git://github.com/thoughtbot/clearance.git'
+# sub.plugin 'restful-authentication', :git => 'git://github.com/technoweenie/restful-authentication.git'
+# I hope role_requirement works with clearance too
+  sub.plugin 'role_requirement', :git => 'git://github.com/timcharper/role_requirement.git'
 end
 
 git :submodule => "init"
