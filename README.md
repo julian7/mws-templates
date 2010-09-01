68727b001076195c6259bdeb5be2796e4c3f6c73
Balazs Nagy <julsevern@gmail.com>
2010-09-02 00:17:46 +0200

    Updated to latest gems and ruby 1.9.2
    More specifically:
    
    * devise is out
    * mysql2 is the new mysql gem
    * formtastic (beta) gem is out
    * added god, unicorn
    * replaced jquery generation with jquery-rails gem
    * tests: added capybara, shoulda, and email_spec, removed test-unit and webrat
    * More specific inploy config
    * Usage updated due to the inability of switching among rvm configs during script run
    
    Signed-off-by: Balazs Nagy <julsevern@gmail.com>

--- README.md
+++ README.md
@@ -2,19 +2,25 @@
 
 ## Usage
 
+    rvm use 1.9.2@<app name> --create
+    gem install --no-ri --no-rdoc rails bundler
     rails new <app name> -J -T -m http://github.com/julian7/mws-templates/raw/master/mws.rb
 
 ## Resources used
 
 * Version control: [git](http://git-scm.com/)
-* Ruby engine: [RVM](http://rvm.beginrescueend.com/)
+* Ruby engine: [RVM](http://rvm.beginrescueend.com/), [Ruby 1.9.2](http://ruby-lang.org/)
 * Template engine: [Haml](http://haml-lang.com/), [Sass](http://sass-lang.com/)
 * Form generator: [Formtastic](http://github.com/justinfrench/formtastic)
-* Javascript: [jQuery](http://www.jquery.com/), [Unobtrusive Javascript for jquery](http://github.com/rails/jquery-ujs/)
+* Javascript: [jQuery](http://www.jquery.com/), [Unobtrusive Javascript for jquery](http://github.com/rails/jquery-ujs/), [jquery-rails](http://rubygems.org/gems/jquery-rails)
 * Application server: [Unicorn](http://unicorn.bogomips.org/)
-* Deployment: [inploy](http://github.com/dcrec1/inploy)
-* Unit test: [rspec](http://rspec.info/) (and soon [shoulda](http://github.com/thoughtbot/shoulda))
-* Behavior test: [cucumber](http://cukes.info/) (and [webrat](http://github.com/brynary/webrat), maybe [capybara](http://github.com/jnicklas/capybara) in the future)
-* Fixtures: [factory_girl](http://github.com/thoughtbot/factory_girl)
+* Watchdog: [God](http://god.rubyforge.org/)
+* Deployment: [Inploy](http://github.com/dcrec1/inploy)
+* Unit test: [RSpec](http://rspec.info/) and [Shoulda](http://github.com/thoughtbot/shoulda)
+* Behavior test: [Cucumber](http://cukes.info/)
+* Web browser emulation: [Capybara](http://github.com/jnicklas/capybara)
+* Fixtures: [Factory Girl](http://github.com/thoughtbot/factory_girl)
 * Authentication: [Devise](http://github.com/plataformatec/devise)
 * Authorization: [CanCan](http://github.com/ryanb/cancan)
+
+For more settings, visit [html5boilerplate.com](http://html5boilerplate.com/).
--- mws.rb
+++ mws.rb
@@ -7,26 +7,13 @@ remove_file 'public/images/rails.png'
 
 # Set up RVM
 create_file '.rvmrc', <<-EnD
-rvm_gemset_create_on_use_flag=1
-rvm gemset use #{app_name} > /dev/null
+rvm use #{ENV['rvm_ruby_string']}@#{app_name} --create > /dev/null
 EnD
 
-# JQuery
-get 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js', 'public/javascripts/jquery.js'
-get 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/jquery-ui.min.js', 'public/javascripts/jquery-ui.js'
-get 'http://github.com/rails/jquery-ujs/raw/master/src/rails.js', 'public/javascripts/rails.js'
-
-initializer 'jquery.rb', <<-EnD
-module ActionView::Helpers::AssetTagHelper
-  remove_const :JAVASCRIPT_DEFAULT_SOURCES
-  JAVASCRIPT_DEFAULT_SOURCES = %w(jquery.js jquery-ui.js rails.js)
-
-  reset_javascript_include_default
-end
-EnD
+run 'rvm rvmrc trust'
 
 # Keep directories in Git
-run "touch {public/images,tmp/{cache,pids,sessions,sockets},log}/.gitkeep"
+run "touch {public/images,tmp/{cache,pids,sessions,sockets},log}/.gitignore"
 
 # Configuration
 ## Database config
@@ -50,66 +37,69 @@ EnD
 
 # Gemfile
 gem 'haml'
-gem 'devise', :git => 'git://github.com/plataformatec/devise.git'
-# Rails 3.0.0.beta4-aware Devise has not been released yet
-#gem 'devise', '>=1.1.rc1'
+gem 'jquery-rails'
+gem 'devise'
 gem 'cancan'
-gem 'will_paginate', '3.0.pre'
-gem 'ruby-mysql', '>= 2.9.2', :require => 'mysql'
-gem 'formtastic', :git => 'http://github.com/justinfrench/formtastic.git', :branch => 'rails3'
-gem 'sqlite3-ruby', '>=1.3.0', :require => 'sqlite3'
+gem 'will_paginate', :git => 'http://github.com/mislav/will_paginate.git', :branch => 'rails3'
+gem 'mysql2'
+gem 'formtastic', '1.1.0.beta'
+gem 'inploy'
+gem 'inploy_godlike'
+gem 'god'
+gem 'unicorn'
 
 append_file 'Gemfile', <<-EnD
-group :development do
+group :development, :test do
+  gem 'sqlite3-ruby', :require => 'sqlite3'
   gem 'rails3-generators'
-  gem 'grit'
-end
-
-group :test do
-  gem 'test-unit', '=1.2.3', :require => 'test/unit'
-  gem 'rspec-rails', '>= 2.0.0.beta.12'
-  gem 'webrat'
+  gem 'rspec-rails', '~> 2.0.0.beta.19'
+  gem 'capybara'
+  gem 'shoulda'
   gem 'cucumber-rails'
   gem 'database_cleaner'
   gem 'factory_girl_rails'
+  gem 'email_spec'
+  # gem 'grit' # for code beautifier TextMate bundle
 end
 EnD
+
+run 'bundle install'
 
 # Deployment
-rakefile 'load_inploy.rake', <<EnD
-begin
-  $:.push Rails.root + 'lib'
-  load 'tasks/inploy.rake'
-rescue LoadError
-  # silently ignore
-end
-EnD
-
 create_file 'config/deploy.rb', <<EnD
-# depoy.template = 'selectbygittag'
+# depoy.template = 'rails3tags'
 # deploy.repository = "git@github.com/username/#{app_name}.git"
 
 case ENV['environment']
 when 'development'
   deploy.application = "#{app_name}"
   deploy.user = 'DEVUSER' # app username
-  deploy.hosts = ['web2.js.hu']
-  deploy.path = '/var/www/#{app_name}.dev.js.hu/apps'
-  deploy.environment = :dev
+  deploy.hosts = ['DEVELOPMENT_APP_SERVER']
+  deploy.path = "/var/www/webapps/#{app_name}"
+  deploy.server = :godlike
+  deploy.god_group = "#{app_name}"
 when 'staging'
   deploy.application = "#{app_name}"
-  deploy.user = 'DEVUSER'
-  deploy.hosts = ['web2.js.hu']
-  deploy.path = '/var/www/#{app_name}.stg.js.hu/apps'
-  deploy.server = :unicorn
+  deploy.user = 'STGUSER'
+  deploy.hosts = ['STAGING_APP_SERVER']
+  deploy.path = "/var/www/webapps/#{app_name}-\#{ENV['environment']}"
+  deploy.server = :godlike
+  deploy.god_group = "#{app_name}"
   # deploy.tag = 'iterationN'
+when 'production'
+  deploy.application = "#{app_name}"
+  deploy.user = 'PRODUSER'
+  deploy.hosts = ['PRODUCTION_APP_SERVER']
+  deploy.path = "/var/www/webapps/#{app_name}-\#{ENV['environment']}"
+  deploy.server = :godlike
+  deploy.god_group = "#{app_name}"
+  # deploy.tag = 'releaseN'
 else
-  deploy.application = 'APP'
+  deploy.application = "#{app_name}"
   deploy.user = 'USERNAME'
-  deploy.hosts = ['web2.js.hu']
+  deploy.hosts = ['OTHER_APP_SERVER']
   deploy.path = '/var/www/HOSTNAME/apps'
   deploy.server = :unicorn
-  # deploy.tag = 'releaseN'
 end
 EnD
 
@@ -146,6 +136,8 @@ inject_into_file 'app/controllers/application_controller.rb', :before => '\nend'
   EnD
 end
 # Views
+## jQuery
+generate 'jquery:install'
 ## Formtastic
 generate 'formtastic:install'
 ## Layout
@@ -154,6 +146,8 @@ create_file 'app/views/layouts/application.html.haml', <<-EnD
 !!! 5
 %html
   %head
+    %meta{:charset => 'utf-8'}
+    %meta{"http-equiv" => 'X-UA-Compatible', :content => 'IE=edge,chrome=1'}
     %title #{app_name.humanize}
     = stylesheet_link_tag :all, :cache => true
     = javascript_include_tag :defaults, :cache => true
@@ -161,10 +155,6 @@ create_file 'app/views/layouts/application.html.haml', <<-EnD
   %body
     = yield
 EnD
-
-run 'gem install bundler'
-run 'bundle install'
-run 'bundle lock'
 
 # Models
 ## CanCan
@@ -217,7 +207,7 @@ def should_be_forbidden_for(group)
   should_be_forbidden
 end
 EnD
-generate 'cucumber:skeleton --webrat --rspec'
+generate 'cucumber:skeleton --capybara --rspec'
 
 git :init
 git :submodule => "init"
